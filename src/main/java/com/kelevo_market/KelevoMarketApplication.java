142c554090573967796b6c56840d3e8ed826b0eb
Patrick Jhonatan Hernandez Blanco <patrickjhonatanh@gmail.com>
2025-08-31 23:43:55 -0600

    Properties configured

---
+++ src/main/java/com/kelevo_market/KelevoMarketApplication.java
@@ -0,0 +1,13 @@
+package com.kelevo_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KelevoMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KelevoMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kelevo_market/KelevoMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kelevo_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KelevoMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
